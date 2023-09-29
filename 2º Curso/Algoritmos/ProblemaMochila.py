2289396100529725db5f580316bc3ad8d84b9cc0
Di3g0E <desclarin@gmail.com>
2023-09-29 14:25:13 +0200

    "Código:ProblemaMochila"

--- 2º Curso/Algoritmos/ProblemaMochila.py
+++ 2º Curso/Algoritmos/ProblemaMochila.py
@@ -1,43 +1,29 @@
-# Meter en la mochila la mayor cantidad de valor para el peso que puedes llevar
+# Problema de la mochila
 
-def suma(lista):
-    valor = 0
-    for i in lista:
-        valor += i
+N = 5       # Cantidad de elementos
+W = 100     # Peso soportable dentro de la mochila
+w = [10, 20, 30, 40, 50]; v = [20, 30, 66, 40, 60]  # Peso y valor de cada elemento
 
-    return valor
+# Calcular la relación valor-peso de cada elemento
+vw = [vi / wi for vi, wi in zip(v, w)]
 
-def mochilaVoraz1(peso, valor, pesoMax):
-    rel_Val_Pes = []
-    mochila = []
-    pesoMochila = 0
-    indicesUsados = []
+# Meter los elementos de mayor valor en la mochila y dividir el que no quepa (para que entre al menos una fracción de su valor)
+vw_copy = vw[:]
+mochila = []
+sumaPesos = 0
+while W != sumaPesos:
+    elem = max(vw_copy)
+    if W > sumaPesos + w[vw.index(elem)]:
+        mochila.append(v[vw.index(elem)])
+        sumaPesos += w[vw.index(elem)]
+        vw_copy.remove(elem)
+    else:
+        frac = (W-sumaPesos) / w[vw.index(elem)]
+        mochila.append(v[vw.index(elem)] * frac)
+        sumaPesos = W
 
-    # Saber relacion valor-peso
-    for i in range(len(peso)):
-        rel_Val_Pes.append(valor[i]/peso[i])
-
-    rel_Val_Pes_Copy = rel_Val_Pes[:]
-
-    # Meter en la mochila por orden de relacion valor-peso
-    while pesoMochila != pesoMax:
-        i = rel_Val_Pes.index(max(rel_Val_Pes_Copy))
-        pesoRestante = pesoMax - pesoMochila
-        if i not in indicesUsados and pesoRestante > peso[i]:
-            mochila.append(valor[i])
-            indicesUsados.append(i)
-            pesoMochila += peso[i]
-            rel_Val_Pes_Copy.remove(max(rel_Val_Pes_Copy))
-
-        elif pesoRestante < peso[i]: # Cuando no queden valores enteros que meter se calcula el espacio sobrante y se parte el objeto
-            espacio = pesoRestante / peso[i]
-            mochila.append(int(valor[i] * espacio))
-            pesoMochila = pesoMax
-
-    return mochila
-
-# Programa Principal:
-peso = [10, 20, 30, 40, 50]
-valor = [20, 30, 66, 40, 60]
-pesoMax = 100
-print("Valor igual a: ", suma(mochilaVoraz1(peso, valor, pesoMax)), ". Los valores de la mochila son: ", mochilaVoraz1(peso, valor, pesoMax))
+# Indicar el valor final de la mochila
+valMochila = 0
+for elem in mochila:
+    valMochila += elem
+print(valMochila)
