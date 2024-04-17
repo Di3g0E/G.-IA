4c36c0fe56f91cb9ab197497d28e721bbd8d86b7
Di3g0E <desclarin@gmail.com>
2024-04-17 15:25:12 +0200

    ED 2

--- Curso_2/Cuatri_2/EstructuraDatos_2/estudioPractica/Grafos.h
+++ Curso_2/Cuatri_2/EstructuraDatos_2/estudioPractica/Grafos.h
@@ -7,29 +7,52 @@
 
 #include <iostream>
 #include <unordered_map>
+#include <vector>
 
-struct Book{
-    std::string title;
-    std::string author;
-    int year;
-    int npages;
+template <typename T, typename W>
+class BasicGraph {
+public:
+    void addNode(T node) {
+        nodes[node] = {};
+    }   // Añadir nodo
 
+    void setEdge(T sourceNode, T targetNode, W weight) {
+        nodes[sourceNode][targetNode] = weight;
+        nodes[targetNode][sourceNode] = weight; // Grafo no dirigido
+    }   // Añadir arista (con peso)
 
-    auto operator<=>(const Book& rhs) {
-        return title <=> rhs.title;
-    }
+    const std::unordered_map<T, W>& getEdges(T node) const {
+        return nodes[node];
+    }   // Devolver las aristas conectadas a otro nodo
 
-    void mostrar(){
-        std::cout << "Titulo: " << title << "\t" << "Autor: " << author << "\t" << "Año: " << year << "\t" << "Num. paginas: " << npages << "\n";
-    }
+    // No puede ser const porque puede crear la arista vacía si no existe
+    W getEdge(T sourceNode, T targetNode) {
+        if (nodes[sourceNode][targetNode]){
+            return nodes[sourceNode][targetNode];
+        } else return 0;
+    }   // Devolver el peso
+
+    void removeNode(T node) {
+        auto& edges = nodes[node];
+        for (const auto& [targetNode, weight] : edges) {
+            nodes[targetNode].erase(node);
+        }
+        nodes.erase(node);
+    }   // Eliminar nodo
+
+    auto begin() {return nodes.begin();}
+    auto end() {return nodes.end();}
+
+private:
+    std::unordered_map<T, std::unordered_map<T, W>> nodes;
 };
 
-template <>
-        class std::hash<Book> {
-auto operator()(const Book& rhs) {
-return std::hash<std::string>()(rhs.title);
-}
+struct city {
+    std::string name;
+    int population;
+    int suface;
 };
+
 
 
 #endif //ESTUDIOPRACTICA_GRAFOS_H
--- Curso_2/Cuatri_2/EstructuraDatos_2/estudioPractica/main.cpp
+++ Curso_2/Cuatri_2/EstructuraDatos_2/estudioPractica/main.cpp
@@ -8,7 +8,7 @@
 //#include "Iterador_num_par.h"
 //#include "Inorden_iterator.h"
 #include "Grafos.h"
-#include "Preorden_iterator.h"
+// #include "Preorden_iterator.h"
 
 
 
@@ -96,7 +96,7 @@ int main() {
     std::cout << '\n';
      */ // Iteradores
 
-
+    /*
     Book libro1 {"T_1", "A_1", 1000, 100};
     Book libro2 {"T_2", "A_2", 2000, 200};
     Book libro3 {"T_3", "A_3", 3000, 300};
@@ -120,6 +120,51 @@ int main() {
     for (const auto& [key, value]: map){
         std::cout << "map[" << key.title << "] = " << value << "\n";
     }
+    */
+
+    BasicGraph<int, int> graph;
+
+    // Agregar nodos
+    graph.addNode(1);
+    graph.addNode(2);
+    graph.addNode(3);
+    graph.addNode(4);
+    graph.addNode(5);
+
+    // Agregar aristas
+    graph.setEdge(1, 2, 42);
+    graph.setEdge(1, 3, 43);
+    graph.setEdge(1, 4, 44);
+    graph.setEdge(1, 5, 44);
+    graph.setEdge(2, 3, 37);
+    graph.setEdge(3, 4, 34);
+    graph.setEdge(4, 5, 54);
+
+    // Mostrar grafo completo
+    for (const auto& [node, edges] : graph) {
+        std::cout << "Aristas del nodo " << node << ":" << "\n";
+        for (const auto& [targetNode, weight] : edges) {
+            std::cout << "  - Nodo " << targetNode << ": " << ((weight != 0) ? "Conectado" : "No conectado") << "\n";
+        }
+    }
+
+    graph.removeNode(3);
+
+    std::cout << "***********************\n";
+
+    // Mostrar grafo sin nodo 3
+    for (const auto& [node, edges] : graph) {
+        std::cout << "Aristas del nodo " << node << ":" << "\n";
+        for (const auto& [targetNode, weight] : edges) {
+            std::cout << "  - Nodo " << targetNode << ": " << ((weight != 0) ? "Conectado" : "No conectado") << "\n";
+        }
+    }
+
+    std::cout << "***********************\n";
+
+    // GetEdges muestra la arista si existe y sino la crea "vacia"
+    std::cout << "Node 1 - Node 2: " << graph.getEdge(1, 2) << "\n";
+    std::cout << "Node 2 - Node 5: " << graph.getEdge(2, 5) << "\n";
 
     return 0;
 }
